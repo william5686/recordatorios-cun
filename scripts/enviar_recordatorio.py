a67d95611ee497cf4aa1020144c39b9a489b85e1
William Estrada <estrada5686@live.com>
2026-09-14 11:11:51 -0500

    Update enviar_recordatorio.py

--- scripts/enviar_recordatorio.py
+++ scripts/enviar_recordatorio.py
@@ -90,13 +90,7 @@ def construir_mensaje(persona):
         f"Hola {persona['nombre']},\n\n"
         "hoy te corresponde realizar la descarga y actualizacion del ACA.\n\n"
         "Muchas gracias por tu colaboracion 😀\n"
-        "PD:Recuerda son los periodos:• 26V03, 26V04, 26V05
-• 26T03, 26T04, 26T05
-• 26PO3, 26P04, 26P05
-• 26P15, 26P14
-• 26104, 26105
-• 26ES3, 26ES4, 26ES5
-• 2026C, 2026D, 2026Q🤔\n"
+        "PD:Recuerda LOS NUEVOS PERIODOS V/T/P05 y 2026D🤔\n"
     )
     return asunto, cuerpo
 
